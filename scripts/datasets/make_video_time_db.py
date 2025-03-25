15075642ece780c249e44e97f70ea4ffe5e7bfb3
Daniel Herrera C <daniel.herrera.castro@gmail.com>
2025-03-25 14:31:16 +0100

    VideoDB for night videos

--- scripts/datasets/make_video_time_db.py
+++ scripts/datasets/make_video_time_db.py
@@ -20,6 +20,18 @@ CAMERA_NAMES = [
     "zag_elp_cam_019",
 ]
 
+CAMERA_PREFIXES = {
+    "ELP - Sandbox_mit_Buero-": "zag_elp_cam_017",
+    "ELP - Sandbox_mit_MGMT-": "zag_elp_cam_018",
+    "ELP - Sandbox_ohne_Büro-": "zag_elp_cam_016",
+    "ELP - Sandbox_ohne_MGMT-": "zag_elp_cam_019",
+    "zag_elp_cam_016-": "zag_elp_cam_016",
+    "zag_elp_cam_017-": "zag_elp_cam_017",
+    "zag_elp_cam_018-": "zag_elp_cam_018",
+    "zag_elp_cam_019-": "zag_elp_cam_019",
+}
+CAMERA_PREFIXES = {k.lower().replace("-", "_"): v for k, v in CAMERA_PREFIXES.items()}
+
 
 @dataclass
 class VideoInfo:
@@ -40,16 +52,19 @@ def parse_video_name(filename: Path) -> VideoInfo:
 
     # Parse {camera}
     filename_underscore = filename.replace("-", "_")
-    camera_prefixes = [
-        camera_name
-        for camera_name in CAMERA_NAMES
-        if filename_underscore.startswith(camera_name)
+    camera_names = [
+        CAMERA_PREFIXES[prefix]
+        for prefix in CAMERA_PREFIXES.keys()
+        if filename_underscore.startswith(prefix)
     ]
-    assert len(camera_prefixes) == 1
-    camera_prefix = camera_prefixes[0]
+    if len(camera_names) != 1:
+        raise RuntimeError(
+            f"Filename {str(filename)} could not be parsed: camera prefix error"
+        )
+    camera_name = camera_names[0]
 
     # Remove camera prefix
-    filename = filename[len(camera_prefix) + 1 :]
+    filename = filename[len(camera_name) + 1 :]
 
     # Parse {date}
     if filename[2] == ".":
@@ -78,9 +93,19 @@ def parse_video_name(filename: Path) -> VideoInfo:
     video = cv2.VideoCapture(original_filename)
     assert video.isOpened()
     frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    # Find end
+    video.set(cv2.CAP_PROP_POS_FRAMES, frame_count - 2)
+    video.read()  # Read dummy frame to update props
+    video_length_s = video.get(cv2.CAP_PROP_POS_MSEC) / 1000
+    if video_length_s == 0:
+        fps = 25
+        video_length_s = frame_count / fps
+    else:
+        fps = frame_count / video_length_s
     video = None
 
-    if "hour1" in fields:
+    if False and "hour1" in fields:
         # Filename has end time, calculate fps
         end_day = fields["day"]
         if fields["hour0"] == 23 and fields["hour1"] < 23:
@@ -96,9 +121,10 @@ def parse_video_name(filename: Path) -> VideoInfo:
         fps = frame_count / (end_time - start_time).total_seconds()
     else:
         # No end time in filename. Assume fps and calculate end time
-        fps = 25.0026
-        end_time = start_time + datetime.timedelta(seconds=frame_count / fps)
-    return VideoInfo(original_filename, camera_prefix, start_time, end_time, fps)
+        # end_time = start_time + datetime.timedelta(seconds=frame_count / fps)
+        end_time = start_time + datetime.timedelta(seconds=video_length_s)
+    print(f"Video={original_filename.name}, length={end_time - start_time}")
+    return VideoInfo(original_filename, camera_name, start_time, end_time, fps)
 
 
 def parse_video_files(files: list[Path]) -> dict[str, list[VideoInfo]]:
@@ -108,6 +134,7 @@ def parse_video_files(files: list[Path]) -> dict[str, list[VideoInfo]]:
 
     pbar = ECounter(total=len(files), desc="Parsing videos", unit="file")
     for video_file in pbar(files):
+        pbar.desc = str(video_file)
         info = parse_video_name(video_file)
         video_info_by_camera[info.camera].append(info)
 
