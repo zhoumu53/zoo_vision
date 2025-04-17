e6aaef746702289cb6a85706c3d25f11531697d5
Daniel Herrera C <daniel.herrera.castro@gmail.com>
2025-04-17 14:04:09 +0200

    Tensorrt experiments (fails)

--- training/compile_all_models.py
+++ training/compile_all_models.py
@@ -1,4 +1,5 @@
 import torch
+import torch_tensorrt
 import argparse
 from pathlib import Path
 from enlighten.counter import Counter as ECounter
@@ -20,17 +21,65 @@ def compile_model(weights_path: Path, output_path: Path) -> None:
     model = load_model(weights_path)
     device = torch.device("cuda")
     model = model.to(device)
+
+    image_size = model.input_image_size
+    sample_inputs = torch.rand(
+        (1, 3, image_size["height"], image_size["width"]),
+        dtype=torch.float32,
+        device=device,
+    )
+
     if not model.supports_jit:
-        image_size = model.input_image_size
-        sample_inputs = torch.rand(
-            (1, 3, image_size["height"], image_size["width"]),
-            dtype=torch.float32,
-            device=device,
-        )
+        print(f"Tracing module with image input: [{sample_inputs.shape}]")
         traced_module = torch.jit.trace(model, [sample_inputs], strict=False)
     else:
         traced_module = torch.jit.script(model)
-    traced_module.save(output_path)
+
+    # Compile with tensorrt
+    # None of this works, keep it at 0 to avoid tensorrt
+    method = 0
+    if method == 0:
+        trt_model = traced_module
+    else:
+        print(f"Compiling with tensorrt...")
+
+        trt_input = torch_tensorrt.Input(
+            [1, 3, image_size["height"], image_size["width"]],
+            dtype=torch.float,
+        )
+        if method == 1:
+            exp_model = torch.export.export(model, tuple([sample_inputs]))
+            trt_model = torch_tensorrt.dynamo.compile(exp_model, [sample_inputs])
+        elif method == 2:
+            trt_model = torch_tensorrt.ts.compile(
+                traced_module,
+                inputs=[trt_input],
+                truncate_long_and_double=True,
+            )
+        elif method == 3:
+            spec = {
+                "forward": torch_tensorrt.ts.TensorRTCompileSpec(
+                    **{
+                        "inputs": [trt_input],
+                        "enabled_precisions": {torch.int, torch.float, torch.half},
+                        "truncate_long_and_double": True,
+                        "refit": False,
+                        "debug": False,
+                        "device": {
+                            "device_type": torch_tensorrt.DeviceType.GPU,
+                            "gpu_id": 0,
+                            "dla_core": 0,
+                            "allow_gpu_fallback": True,
+                        },
+                        "capability": torch_tensorrt.EngineCapability.STANDARD,
+                        "num_avg_timing_iters": 1,
+                    }
+                )
+            }
+            trt_model = torch._C._jit_to_backend("tensorrt", traced_module, spec)
+
+    # Save
+    torch.jit.save(trt_model, output_path)
     print(f"Saved to {output_path}")
 
     if hasattr(model, "vit"):
