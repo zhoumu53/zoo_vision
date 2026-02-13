97230b199709d4b79649a516b6c3fce1de17beea
zhoumu53 <jassica.zm53@gmail.com>
2026-02-13 12:48:07 +0100

    add pythonpath

--- activate_ros.sh
+++ activate_ros.sh
@@ -11,5 +11,5 @@ export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
 export RMW_FASTRTPS_USE_QOS_FROM_XML=1
 export FASTRTPS_DEFAULT_PROFILES_FILE="$PWD/zoo_vision_ros2/src/fast_dds_config.xml"
 export LD_LIBRARY_PATH=$PWD/build/RelWithDebInfo/src/zoo_msgs:${LD_LIBRARY_PATH}
-export CLICOLOR_FORCE=1 # Enable color on ninja build
+export CLICOLOR_FORCE=1 # Enable color on ninja build 
 export PYTHONPATH="$PYTHONPATH:$PWD"
